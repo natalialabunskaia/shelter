01432ce1b53710639a93294bbbdeb7cfa6ec3d66
Natalia Labunskaia <nlabunskya@gmail.com>
2026-06-22 12:54:54 +0400

    chore: set up Prettier and ESLint

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2024,
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "prefer-const": "error",
+      "no-var": "error",
+    },
+  },
+  eslintConfigPrettier,
+];
